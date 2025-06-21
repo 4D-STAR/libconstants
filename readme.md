df5e92b0e91d66e8f2731a864d9e261d173edcbd
Emily Boudreaux <thomas@boudreauxmail.com>
2025-06-21 05:52:43 -0400

    build(libconstants): added working test system

---
+++ readme.md
@@ -0,0 +1,5 @@
+# libconstants
+
+libconstants is the authoritative source for physical constants for the SERiF project.
+
+This has been broken out of the main serif project to allow for more modularity
---
+++ tests/constants/constTest.cpp
@@ -0,0 +1,109 @@
+#include <gtest/gtest.h>
+#include "const.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <sstream>
+
+/**
+ * @file constTest.cpp
+ * @brief Unit tests for the const class.
+ */
+
+/**
+ * @brief Test suite for the const class.
+ */
+class constTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        serif::constant::Constants::getInstance();
+    }
+};
+
+/**
+ * @test Verify default constructor initializes correctly.
+ */
+TEST_F(constTest, DefaultConstructor) {
+    EXPECT_NO_THROW(serif::constant::Constants::getInstance());
+}
+
+/**
+ * @test Verify constructor initializes with provided data and metadata.
+ */
+TEST_F(constTest, isLoaded) {
+
+    EXPECT_NO_THROW(serif::constant::Constants::getInstance().isLoaded());
+}
+
+/**
+ * @test Verify get method returns the correct constant.
+ */
+TEST_F(constTest, GetMethod) {
+    serif::constant::Constants& obj = serif::constant::Constants::getInstance();
+    EXPECT_DOUBLE_EQ(obj.get("c").value, 2.99792458e10);
+    EXPECT_EQ(obj.get("c").unit, "cm / s");
+    EXPECT_DOUBLE_EQ(obj.get("c").uncertainty, 0.0);
+    EXPECT_EQ(obj.get("c").reference, "CODATA2022");
+}
+
+/**
+ * @test Verify [] opperators returns the correct constant.
+ */
+TEST_F(constTest, SubscriptOperator) {
+    serif::constant::Constants& obj = serif::constant::Constants::getInstance();
+    EXPECT_DOUBLE_EQ(obj["c"].value, 2.99792458e10);
+    EXPECT_EQ(obj["c"].unit, "cm / s");
+    EXPECT_DOUBLE_EQ(obj["c"].uncertainty, 0.0);
+    EXPECT_EQ(obj["c"].reference, "CODATA2022");
+}
+
+/**
+ * @test Verify that the has method returns the correct values
+ */
+TEST_F(constTest, HasMethod) {
+    serif::constant::Constants& obj = serif::constant::Constants::getInstance();
+
+    EXPECT_TRUE(obj.has("c"));
+    EXPECT_FALSE(obj.has("c4"));
+    EXPECT_TRUE(obj.has("hbar"));
+}
+
+TEST_F(constTest, KeysMethod) {
+    serif::constant::Constants& obj = serif::constant::Constants::getInstance();
+    std::set<std::string> checkKeys;
+    checkKeys.insert("c");
+    checkKeys.insert("wienK");
+    checkKeys.insert("hbar");
+    checkKeys.insert("g_h");
+    checkKeys.insert("R_sun");
+
+    std::set<std::string> keys = obj.keys();
+
+    for (const auto& key : checkKeys) {
+        bool found = keys.find(key) != keys.end();
+        EXPECT_TRUE(found);
+    }
+
+    std::set<std::string> checkBadKeys;
+    checkBadKeys.insert("c4");
+    checkBadKeys.insert("wienK4");
+    checkBadKeys.insert("hbar4");
+    checkBadKeys.insert("g_h4");
+    checkBadKeys.insert("R_sun4");
+
+    for (const auto& key : checkBadKeys) {
+        bool found = keys.find(key) != keys.end();
+        EXPECT_FALSE(found);
+    }
+}
+
+TEST_F(constTest, StreamOperator) {
+    serif::constant::Constants& obj = serif::constant::Constants::getInstance();
+    std::ostringstream os;
+
+    os << obj.get("c");
+    std::string expected = "<speed of light in vacuum: 2.99792e+10±0 cm / s (CODATA2022)>\n";
+
+    EXPECT_EQ(os.str(), expected);
+}
